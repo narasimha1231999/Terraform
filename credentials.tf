5652e8150f1bbdf4a6398aaf81f32b96aeac152b
narasimha1231999 <narasimha1231999@gmail.com>
2026-05-20 11:17:18 +0530

    add

---
+++ credentials.tf
@@ -0,0 +1,6 @@
+##########AWS CONFIGURATION############
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIA37GGFHQ3ZEYHOAOB"
+  secret_key = "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"
+}
